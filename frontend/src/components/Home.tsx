ea69fb68d544dff1a539af5306b45a7bb5c9c73f
PloychompooWongchanthat <poychom@gmail.com>
2022-02-12 19:16:54 +0700

    edit login

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,6 +1,7 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-
+import { ImageList, styled, Paper, Grid, Box } from "@material-ui/core";
+import ImageListItem from '@material-ui/core/ImageListItem';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
@@ -14,7 +15,12 @@ const useStyles = makeStyles((theme: Theme) =>
     },
   })
 );
-
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 function Home() {
   const classes = useStyles();
 
@@ -22,11 +28,8 @@ function Home() {
     <div>
       <Container className={classes.container} maxWidth="md">
         <h1 style={{ textAlign: "center" }}>ระบบห้องสมุด</h1>
-        <h3>Requirements</h3>
-        <p>
-        ระบบห้องสมุด
-
-        </p>
+        <img src="https://images.pexels.com/photos/1319855/pexels-photo-1319855.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          width="900px"></img>
       </Container>
     </div>
   );
